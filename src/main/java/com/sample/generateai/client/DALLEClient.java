68b9ca8b004757bab1853302047e2b6690df22b9
Yu MOTOJIMA <51645202+motojimay@users.noreply.github.com>
2024-10-08 14:56:08 +0900

    little fix

--- src/main/java/com/sample/generateai/client/DALLEClient.java
+++ src/main/java/com/sample/generateai/client/DALLEClient.java
@@ -73,4 +73,5 @@ public class DALLEClient {
             this.size = size;
         }
 
-    }}
+    }
+}
--- src/main/java/com/sample/generateai/repository/GeneratedImageRepository.java
+++ src/main/java/com/sample/generateai/repository/GeneratedImageRepository.java
@@ -1,7 +1,6 @@
 package com.sample.generateai.repository;
 
 import com.sample.generateai.entity.ImageEntity;
-import com.sample.generateai.model.Image;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import reactor.core.publisher.Mono;
 
